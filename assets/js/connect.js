226574b570eeca8a516c9ab4ec1b6281e4df44ba
rykush <actushima@gmail.com>
2026-04-17 12:30:07 +0300

    fix connect foot and head

--- assets/js/connect.js
+++ assets/js/connect.js
@@ -1,11 +1,11 @@
-fetch('../complemen/header.html')
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById('header').innerHTML = data;
-    });
+fetch('complemen/header.html')
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById('header').innerHTML = data;
+  });
 
-fetch('../complemen/footer.html')
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById('footer').innerHTML = data;
-    });
+fetch('complemen/footer.html')
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById('footer').innerHTML = data;
+  });
